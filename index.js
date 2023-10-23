ddb21f4e5d166f8f1a7391fe41522561db9ea70c
Rahul Sharma <sharmarahul8848@gmail.com>
2023-10-23 23:47:10 +0530

    Setup the directory structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", require("./routes"));
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error starting the app ${err}`);
+  }
+  console.log(`App running on port ${port}`);
+});
